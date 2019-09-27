25d2dfbc64d7bdce56b927909ff5a027f8ac182f
Rhonda Kremer <rhondakremer@gmail.com>
2019-09-27 16:04:20 -0400

    updated schema and example.js with table/model info

--- models/schema.sql
+++ models/schema.sql
@@ -1,11 +1,2 @@
 DROP DATABASE IF EXISTS employers_db;
 CREATE DATABASE employers_db;
-
-CREATE TABLE IF NOT EXISTS `employers_db`.`employers` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `employer_name` VARCHAR(45) NOT NULL,
-  `employer_email` VARCHAR(45) NOT NULL,
-  `invoice_amount` DECIMAL(10,2) NOT NULL,
-  `paid_status` TINYINT(1) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
