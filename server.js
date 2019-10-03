2b5942b8013fa6fb1e4eaed5d8ca2dbc33412984
Bob Ross <51304258+happyliltrees@users.noreply.github.com>
2019-10-02 20:44:44 -0400

    fixed force-true/false in sequelize

--- server.js
+++ server.js
@@ -26,12 +26,12 @@ require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 require("./routes/apiRoutesInvoice")(app);
 
-var syncOptions = { force: false };
+var syncOptions = { force: true };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
 if (process.env.NODE_ENV === "test") {
-  syncOptions.force = false;
+  syncOptions.force = true;
 }
 
 // Starting the server, syncing our models ------------------------------------/
